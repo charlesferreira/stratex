51be1aaabed753218d923f129d1465a4dfae7386
Charles Ferreira <charles.dev@live.com>
2016-11-10 19:57:58 -0200

    Commit inicial.

---
+++ Assets/Puzzle/Scripts/Puzzle.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class Puzzle : MonoBehaviour {
+
+    public Transform alliedShip;
+
+    PuzzleInput input;
+    ShipInterface ship;
+
+	void Start () {
+        input = GetComponent<PuzzleInput>();
+        ship = GetComponent<ShipInterface>();
+	}
+	
+	void Update () {
+	}
+}
