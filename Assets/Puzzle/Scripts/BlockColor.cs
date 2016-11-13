d134c04f8bbaa2854cdc6ef6defb80cd7e982bba
Rafael Forbeck <rafaelforbeck@gmail.com>
2016-11-12 22:47:35 -0200

    Script de movimento de objetos com easing, posicionamento dos blocos na grid e teste com texturas.

--- Assets/Puzzle/Scripts/BlockColor.cs
+++ Assets/Puzzle/Scripts/BlockColor.cs
@@ -1,7 +1,4 @@
-﻿using UnityEngine;
-using System.Collections;
-
-public enum BlockColor {
+﻿public enum BlockColor {
 
     Red, Yellow
 }
---
+++ Assets/Puzzle/Scripts/MovementType.cs
@@ -0,0 +1,4 @@
+﻿public enum MovementType {
+
+    Linear, QuadraticIn, QuadraticOut, QuadraticInOut
+}
