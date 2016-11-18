2bdef52367bb42258bf50a2da75fe1869eaaaa92
Charles Ferreira <charles.dev@live.com>
2016-11-18 17:35:12 -0200

    Criado script rudimentar da câmera que segue as naves.

---
+++ Assets/Space/Scripts/CameraFollowShips.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using UnityEngine;
+
+public class CameraFollowShips : MonoBehaviour {
+
+    [Header("References")]
+    public Camera spaceCamera;
+    public Transform ship1;
+    public Transform ship2;
+
+    [Header("Settings")]
+    [Range(0, 10)]
+    public int weight;
+    [Range(0, 1)]
+    public float damping;
+
+    int TotalWeight { get { return weight + 2; } }
+
+    Vector2 target;
+
+    void Update() {
+        target = (ship1.position + ship2.position) / TotalWeight;
+        transform.position = Vector2.Lerp(transform.position, target, damping);
+    }
+}
