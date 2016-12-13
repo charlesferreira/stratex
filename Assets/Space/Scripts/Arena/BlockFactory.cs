326f82729b22d40f07bf460e448947af37f23565
Charles Ferreira <charles.dev@live.com>
2016-12-13 21:52:31 -0200

    Fazendo surgir as peças com os sprites "certos". Criando prefab para o bloco gerado na arena.

--- Assets/Space/Scripts/Arena/BlockFactory.cs
+++ Assets/Space/Scripts/Arena/BlockFactory.cs
@@ -1,58 +1,53 @@
 ﻿using UnityEngine;
 using Common.Math;
 
-namespace Space {
-    namespace Arena {
-        namespace BlocksDistribution {
-            public class BlockFactory : MonoBehaviour {
+namespace Space.Arena.BlocksDistribution {
+    public class BlockFactory : MonoBehaviour {
 
-                // Inspector
-                public Transform blockPrefab;
+        // Inspector
+        public SpaceBlock blockPrefab;
 
-                // Private
-                Histogram histogram;
-                int mapWidth;
-                int mapHeight;
+        // Private
+        Histogram histogram;
+        int mapWidth;
+        int mapHeight;
 
-                // Static
-                static BlockFactory instance;
-                public static BlockFactory Instance {
-                    get {
-                        if (instance == null)
-                            instance = FindObjectOfType<BlockFactory>();
-                        return instance;
-                    }
-                }
-
-                // Lyfe cycle methods
-                void Start() {
-                    var mg = GetComponent<MapGenerator>();
-                    histogram = mg.Histogram;
-                    mapWidth = mg.GetOutputImage().width;
-                    mapHeight = mg.GetOutputImage().height;
-                }
-
-                // Public methods
-                public void CreateBlock(BlockInfo info) {
-                    var go = (Transform)Instantiate(blockPrefab, transform);
-                    var position = RandomPosition();
-                    go.localPosition = position;
-                }
-
-                // Private methods
-                private Vector2 RandomPosition() {
-                    var value = (uint)Random.Range(1, histogram.MaxValue);
-                    var index = histogram.Search(value);
-
-                    var coords = new Vector2(
-                        (index % mapWidth) + Random.Range(0f, 1f),
-                        (index / mapWidth) + Random.Range(0f, 1f));
-
-                    var halfMapSize = new Vector2(mapWidth, mapHeight) / 2f;
-                    return coords - halfMapSize;
-                }
+        // Static
+        static BlockFactory instance;
+        public static BlockFactory Instance {
+            get {
+                if (instance == null)
+                    instance = FindObjectOfType<BlockFactory>();
+                return instance;
             }
+        }
 
+        // Lyfe cycle methods
+        void Start() {
+            var mg = GetComponent<MapGenerator>();
+            histogram = mg.Histogram;
+            mapWidth = mg.GetOutputImage().width;
+            mapHeight = mg.GetOutputImage().height;
+        }
+
+        // Public methods
+        public void CreateBlock(BlockInfo info) {
+            var block = (SpaceBlock)Instantiate(blockPrefab, transform);
+            block.Init(info.spaceSprite, RandomPosition());
+        }
+
+        // Private methods
+        private Vector2 RandomPosition() {
+            var value = (uint)Random.Range(1, histogram.MaxValue);
+            var index = histogram.Search(value);
+
+            var coords = new Vector2(
+                (index % mapWidth) + Random.Range(0f, 1f),
+                (index / mapWidth) + Random.Range(0f, 1f));
+
+            var halfMapSize = new Vector2(mapWidth, mapHeight) / 2f;
+            return coords - halfMapSize;
         }
     }
+
 }
---
+++ Assets/Space/Scripts/Arena/SpaceBlock.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+namespace Space.Arena.BlocksDistribution {
+    public class SpaceBlock : MonoBehaviour {
+        public void Init(Sprite sprite, Vector2 position) {
+            var sr = GetComponent<SpriteRenderer>();
+            sr.sprite = sprite;
+
+            transform.localScale *= sr.sprite.pixelsPerUnit / 100f;
+            transform.localPosition = position;
+        }
+    }
+}
