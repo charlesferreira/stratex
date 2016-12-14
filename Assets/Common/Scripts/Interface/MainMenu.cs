9c534936474cf89de0ab75614c51efde271f7e03
Rafael Forbeck <rafaelforbeck@gmail.com>
2016-12-14 18:46:37 -0200

    Início da implementação do menu de pause

---
+++ Assets/Common/Scripts/Interface/MainMenu.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using UnityEngine.UI;
+
+public class MainMenu : MonoBehaviour {
+
+    List<Button> buttons = new List<Button>();
+    MainMenuInput input;
+    int currentIndexButton = 0;
+
+    void Start()
+    {
+        input = GetComponent<MainMenuInput>();
+
+        foreach (Transform child in transform)
+        {
+            buttons.Add(child.GetComponent<Button>());
+        }
+    }
+
+    void Update()
+    {
+        if (!Pause.Instance.pause)
+        {
+            currentIndexButton = 0;
+            return;
+        }
+
+        if (input.Up)
+        {
+            currentIndexButton = (buttons.Count + currentIndexButton - 1) % buttons.Count;
+        }
+        else if (input.Down)
+        {
+            currentIndexButton = (currentIndexButton + 1) % buttons.Count;
+        }
+
+        buttons[currentIndexButton].Select();
+
+        if (input.ConfirmButton)
+        {
+            buttons[currentIndexButton].onClick.Invoke();
+        }
+        if (input.CancelButton)
+        {
+            Pause.Instance.DoPause();
+        }
+    }
+}
---
+++ Assets/Common/Scripts/Interface/Pause.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+
+public class Pause : MonoBehaviour {
+
+    public Joystick joystick;
+
+    public bool pause = false;
+
+    static Pause instance;
+    public static Pause Instance
+    {
+        get
+        {
+            if (instance == null)
+                instance = FindObjectOfType<Pause>();
+            return instance;
+        }
+    }
+
+    void Start () {
+        transform.GetChild(0).gameObject.SetActive(false);
+    }
+
+    void Update () {
+        if (Input.GetButtonDown(joystick.StartButton))
+        {
+            DoPause();
+        }
+	}
+
+    public void DoPause()
+    {
+        pause = !pause;
+
+        if (pause)
+        {
+            Time.timeScale = 0;
+            transform.GetChild(0).gameObject.SetActive(true);
+        }
+        else
+        {
+            Time.timeScale = 1;
+            transform.GetChild(0).gameObject.SetActive(false);
+        }
+    }
+}
