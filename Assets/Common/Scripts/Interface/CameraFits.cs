942d7adb6735f934e3a0401744cae8de785d97e0
Rafael Forbeck <rafaelforbeck@gmail.com>
2017-01-31 21:56:39 -0200

    Ajustes de câmeras

--- Assets/Common/Scripts/Interface/CameraFits.cs
+++ Assets/Common/Scripts/Interface/CameraFits.cs
@@ -4,17 +4,34 @@ using System.Collections;
 public class CameraFits : MonoBehaviour {
 
     Camera camera;
-	// Use this for initialization
-	void Start () {
+    int displayHeight = 720;
+    int displayWidth = 1280;
+
+    int lastScreenWidth;
+    int lastScreenHeight;
+
+    // Use this for initialization
+    void Start() {
         camera = GetComponent<Camera>();
-        print(camera.pixelWidth);
-        print(camera.pixelHeight);
+
+        SetCameraSize();
     }
-	
-	// Update is called once per frame
-	void Update () {
-        camera.orthographicSize = 1280 * Screen.height / Screen.width * 0.5f;
 
+    // Update is called once per frame
+    void Update()
+    {
+        if (lastScreenHeight != Screen.height || lastScreenWidth != Screen.width)
+            SetCameraSize();
+    }
 
+    private void SetCameraSize()
+    {
+        if (Screen.height / Screen.width > displayHeight / 1280)
+            camera.orthographicSize = 1280 * Screen.height / Screen.width * 0.5f;
+        else
+            camera.orthographicSize = displayHeight * 0.5f;
+
+        lastScreenWidth = Screen.width;
+        lastScreenHeight = Screen.height;
     }
 }
