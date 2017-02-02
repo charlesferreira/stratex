52b010c59fecbd0fe866423d7a5e2c72cd3647a4
Rafael Forbeck <rafaelforbeck@gmail.com>
2017-02-01 23:35:00 -0200

    Continuação do sistema de partículas do puzzle

--- Assets/Puzzle/Scripts/Block/MatchingState.cs
+++ Assets/Puzzle/Scripts/Block/MatchingState.cs
@@ -3,6 +3,7 @@
 public class MatchingState : IBlockState
 {
     private readonly StatePatternBlock block;
+    bool start = false;
 
     public MatchingState(StatePatternBlock statePatternBlock)
     {
@@ -11,7 +12,15 @@ public class MatchingState : IBlockState
 
     public void Update()
     {
-        Object.Destroy(block.gameObject);
+        if (!start)
+        {
+            Object.Destroy(block.gameObject);
+
+            //Object.Instantiate(block.blockParticle, block.transform.position + new Vector3(0, 0, -1), Quaternion.identity, block.transform);
+            //block.GetComponent<SpriteRenderer>().enabled = false;
+            //Object.Destroy(block.gameObject, 2f);
+            //start = true;
+        }
     }
 
     public void ToFallingState()
