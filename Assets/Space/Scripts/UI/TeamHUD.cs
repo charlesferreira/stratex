839ce7438b725c33f60f4be02f2a6e7dcc477a93
Charles Ferreira <charles.dev@live.com>
2017-02-04 18:46:42 -0200

    Adicionada borda neon à HUD e colorida conforme a cor do time.

--- Assets/Space/Scripts/UI/TeamHUD.cs
+++ Assets/Space/Scripts/UI/TeamHUD.cs
@@ -1,12 +1,14 @@
-﻿using System;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.UI;
 
-public class ScoreUI : MonoBehaviour, IScoreObserver {
+public class TeamHUD : MonoBehaviour, IScoreObserver {
 
     public Text allyScore;
     public Text enemyScore;
-    
+    public RawImage neonBorder;
+    public RawImage neonAllyDetail;
+    public RawImage neonEnemyDetail;
+
     TeamFlags allyFlag;
 
     void Start() {
@@ -19,9 +21,12 @@ public class ScoreUI : MonoBehaviour, IScoreObserver {
 
         allyScore.text = score;
         allyScore.color = allyInfo.color;
+        neonBorder.color = allyInfo.color;
+        neonAllyDetail.color = allyInfo.color;
 
         enemyScore.text = score;
         enemyScore.color = enemyInfo.color;
+        neonEnemyDetail.color = enemyInfo.color;
     }
 
     public void ScoreHasChanged(TeamFlags flag, int score) {
